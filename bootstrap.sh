1b1f0cdfd782363302e393285dba1f5782415611
vikingmachine <evan@evanw.net>
2018-11-03 21:21:29 +0100

    Add files via upload

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
+unzip packer_1.3.2_linux_amd64.zip
+mv packer /usr/sbin/
+packer build build.json
+docker run --name dev -i -t ewilliams:ewilliams
+
