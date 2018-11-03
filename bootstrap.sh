af675be370de718f2ba6543b3e4ababf02a2edba
vikingmachine <evan@evanw.net>
2018-11-03 21:43:54 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
 wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 unzip packer_1.3.2_linux_amd64.zip
-sudo mv packer /usr/local/
+sudo mv packer /usr/local/bin/
 packer build build.json
 docker run --name dev -i -t $USER:$USER
 
