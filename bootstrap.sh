9970a8dd7a5d1828db291ea17407b256deb653e1
vikingmachine <evan@evanw.net>
2018-11-03 21:39:53 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
+wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 unzip packer_1.3.2_linux_amd64.zip
 sudo mv packer /usr/local/
 packer build build.json
