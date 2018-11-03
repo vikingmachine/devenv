fa77730d751a40135b93168247c381a7a59eac22
vikingmachine <evan@evanw.net>
2018-11-03 23:06:32 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 unzip packer_1.3.2_linux_amd64.zip
 sudo mv packer /usr/local/bin/
 packer build build.json
-docker run --name dev -it $USER:ws-dev
+docker run --name dev -it $USER:aws-dev
 
