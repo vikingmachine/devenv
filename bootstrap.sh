2adf94ae541d3543f0c66ff9caea725e4091390d
vikingmachine <evan@evanw.net>
2018-11-04 16:19:48 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo systemctl enable docker
 wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
 wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 unzip packer_1.3.2_linux_amd64.zip
