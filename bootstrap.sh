58ce39a6f3687ede2a633bc29b776bfc20beabc9
vikingmachine <evan@evanw.net>
2018-11-05 16:04:09 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,6 @@ wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 wget https://raw.githubusercontent.com/vikingmachine/devenv/master/configure.sh
 unzip packer_1.3.2_linux_amd64.zip
 sudo mv packer /usr/local/bin/
-sudo PACKER_LOG=1 packer build -var "aws_access_key=$aws_access_key" "aws_secret_key=$aws_secret_key" build.json
+sudo PACKER_LOG=1 packer build -var "aws_access_key=$aws_access_key" -var "aws_secret_key=$aws_secret_key" build.json
 sudo docker run --name dev -it local:aws-dev
 
