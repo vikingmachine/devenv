c5e0f716e78bd9d733869ebfdbca26e1e24b40b1
vikingmachine <evan@evanw.net>
2018-11-05 14:23:15 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,8 +3,8 @@
 # Setup Basefarm Session Tools
 
 apt update
-apt -y install curl nano unzip wget openssl python3 python3-pip openssh-client
-pip3 install awscli --upgrade
+apt -y install curl nano unzip wget openssl python python-pip openssh-client
+pip install awscli --upgrade
 wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
 wget https://raw.githubusercontent.com/basefarm/aws-session-tool/master/session-tool.sh
 unzip terraform_0.11.8_linux_amd64.zip
