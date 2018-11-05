6d986a7ff052c9a60e35f7fadff390a483841f6c
vikingmachine <evan@evanw.net>
2018-11-05 15:35:57 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 echo Please enter your AWS Access Key ID:
-read aak
+read aws_access_key
 echo Please enter your AWS Secret Access Key:
-read asak
-
-export AWS_ACCESS_KEY_ID="$aak"
-export AWS_SECRET_ACCESS_KEY="$asak"
+read aws_secret_key
 
 
 if [ -x "$(command -v docker)" ]; then
@@ -23,6 +20,6 @@ wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 wget https://raw.githubusercontent.com/vikingmachine/devenv/master/configure.sh
 unzip packer_1.3.2_linux_amd64.zip
 sudo mv packer /usr/local/bin/
-sudo PACKER_LOG=1 packer build build.json
+sudo PACKER_LOG=1 packer build -var "aws_access_key=$aws_access_key" "aws_secret_key=$aws_secret_key" build.json
 sudo docker run --name dev -it local:aws-dev
 
