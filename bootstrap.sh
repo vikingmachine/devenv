94c3357fad5ca09a2fbaba65fc2246c702301a6f
vikingmachine <evan@evanw.net>
2018-11-05 14:26:03 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,13 @@ export AWS_ACCESS_KEY_ID="$aak"
 export AWS_SECRET_ACCESS_KEY="$asak"
 
 
-curl -fsSL https://get.docker.com -o get-docker.sh
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is already installed"
+else
+   curl -fsSL https://get.docker.com -o get-docker.sh
+fi
+
+
 sh get-docker.sh
 sudo systemctl enable docker
 wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
