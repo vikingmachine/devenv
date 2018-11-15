adafb631c5e3ed6b5059cc41b1e476a27d13a5b8
vikingmachine <evan@evanw.net>
2018-11-15 10:14:30 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,14 +9,15 @@ wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd6
 wget https://raw.githubusercontent.com/basefarm/aws-session-tool/master/session-tool.sh
 unzip terraform_0.11.8_linux_amd64.zip
 echo 'source session-tool.sh' >>~/.bashrc
-source /root/.bashrc
+source ~/.bashrc
 mv terraform session-tool.sh /usr/local/bin/
 echo "$AWS_ACCESS_KEY_ID,$AWS_SECRET_ACCESS_KEY_ID" >> /root/secrets.csv
 #aws configure --profile awsops set aws_access_key_id $AWS_ACCESS_KEY_ID
 #aws configure --profile awsops set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_ID
 #aws configure set default.session_tool_default_profile awsops
 #aws configure set session-tool_bucketname bf-aws-tools-session-tool --profile awsops
-
+source /usr/local/bin/session-tool.sh
+get_session -i /root/secrets.csv -b bf-aws-tools-session-tool -d
 
 # Setup BLESS
 
