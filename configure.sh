6ddc2c0e9f42817bfcfbb3fc02fc86ebf5251e58
vikingmachine <evan@evanw.net>
2018-11-15 12:19:17 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -32,6 +32,7 @@ ssh-keygen -f ~/.ssh/blessid -b 4096 -t rsa -C 'Key for BLESS certificate' -N ''
 ssh-keygen -y -f ~/.ssh/blessid > ~/.ssh/blessid.pub
 touch ~/.ssh/blessid-cert.pub
 ln -s ~/.ssh/blessid-cert.pub ~/.ssh/blessid-cert
+echo 'alias bless=/opt/awsops/python-blessclient/blessclient.run' >>~/.bashrc 
 
 # Make sure that you don't get SSH warnings when logging in to a bastion server (as they sit behind a load balancer)
 touch ~/.ssh/config 
