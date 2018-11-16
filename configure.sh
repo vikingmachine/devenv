c99383b8cc54c447ef60060944fc0f41a5483ac9
vikingmachine <evan@evanw.net>
2018-11-16 15:37:27 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,9 +15,9 @@ mv ~/.bashrc ~/.bashrc.default
 cat <<TEXT >> ~/.bashrc
 . ~/.bashrc.default
 eval ssh-agent
-alias prod="ssh -o StrictHostKeyChecking=no -A $HOSTUSER@linbast.transhub.io"
-alias stage="ssh -o StrictHostKeyChecking=no -A $HOSTUSER@linbast.stage.transhub.io"
-alias test="ssh -o StrictHostKeyChecking=no -A $HOSTUSER@linbast.test.transhub.io"
+alias prod="ssh -A $HOSTUSER@linbast.transhub.io"
+alias stage="ssh -A $HOSTUSER@linbast.stage.transhub.io"
+alias test="ssh -A $HOSTUSER@linbast.test.transhub.io"
 alias bless="/opt/awsops/python-blessclient/blessclient.run"
 source /usr/local/bin/session-tool.sh
 get_session -i /root/secrets.csv -b bf-aws-tools-session-tool -d
