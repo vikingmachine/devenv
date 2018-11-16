36e0930dfeed87a3281094b35348b783f5c6b971
vikingmachine <evan@evanw.net>
2018-11-16 10:49:41 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,11 +8,19 @@ pip install awscli --upgrade
 wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
 wget https://raw.githubusercontent.com/basefarm/aws-session-tool/master/session-tool.sh
 unzip terraform_0.11.8_linux_amd64.zip
+
+#Configure .bashrc
+
 echo 'source session-tool.sh' >>~/.bashrc
 echo 'eval `ssh-agent`' >>~/.bashrc
+echo 'alias bless=/opt/awsops/python-blessclient/blessclient.run' >>~/.bashrc 
+echo 'alias stage="ssh -A $HOSTUSER@linbast.stage.transhub.io"' >>~/.bashrc
+echo 'alias test="ssh -A $HOSTUSER@linbast.test.transhub.io"' >>~/.bashrc
+echo 'alias prod="ssh -A $HOSTUSER@linbast.transhub.io"' >>~/.bashrc
 source ~/.bashrc
 
 #Setup terraform and AWS CLI / Session tools
+
 mv terraform session-tool.sh /usr/local/bin/
 echo "$AWS_ACCESS_KEY_ID,$AWS_SECRET_ACCESS_KEY_ID" >> /root/secrets.csv
 #aws configure --profile awsops set aws_access_key_id $AWS_ACCESS_KEY_ID
@@ -20,7 +28,7 @@ echo "$AWS_ACCESS_KEY_ID,$AWS_SECRET_ACCESS_KEY_ID" >> /root/secrets.csv
 #aws configure set default.session_tool_default_profile awsops
 #aws configure set session-tool_bucketname bf-aws-tools-session-tool --profile awsops
 #source /usr/local/bin/session-tool.sh
-get_session -i /root/secrets.csv -b bf-aws-tools-session-tool -d
+#get_session -i /root/secrets.csv -b bf-aws-tools-session-tool -d
 
 # Setup Bless. This is missing the config of blessclient.cfg. Do that step manually for now
 
@@ -35,11 +43,6 @@ ssh-keygen -f ~/.ssh/blessid -b 4096 -t rsa -C 'Key for BLESS certificate' -N ''
 ssh-keygen -y -f ~/.ssh/blessid > ~/.ssh/blessid.pub
 touch ~/.ssh/blessid-cert.pub
 ln -s ~/.ssh/blessid-cert.pub ~/.ssh/blessid-cert
-echo 'alias bless=/opt/awsops/python-blessclient/blessclient.run' >>~/.bashrc 
-echo 'alias stage="ssh -A $HOSTUSER@linbast.stage.transhub.io"' >>~/.bashrc
-echo 'alias test="ssh -A $HOSTUSER@linbast.test.transhub.io"' >>~/.bashrc
-echo 'alias prod="ssh -A $HOSTUSER@linbast.transhub.io"' >>~/.bashrc
-
 
 # Make sure that you don't get SSH warnings when logging in to a bastion server (as they sit behind a load balancer)
 touch ~/.ssh/config 
