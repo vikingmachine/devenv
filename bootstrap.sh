c1c0b3167770a0f711c543556a61eaa0ab8b1946
Thomas Finstad Larsen <thomas.finstad.larsen@gmail.com>
2018-11-27 14:31:17 +0100

    Stopped bootstrapping from contaminating host system.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+mkdir -p ./build
+cd ./build
+
 docker system prune -a -f
 
 echo Please enter your AWS Access Key ID:
@@ -19,18 +22,17 @@ else
 fi
 
 
-if [ -x "$(command -v packer)" ]; then
+if [ -f "./packer" ]; then
     echo "Packer is already installed"
 else
    wget https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
    unzip packer_1.3.2_linux_amd64.zip
-   mv packer /usr/local/bin/
 fi
-
-
 
 wget https://raw.githubusercontent.com/vikingmachine/devenv/master/build.json
 wget https://raw.githubusercontent.com/vikingmachine/devenv/master/configure.sh
 
-PACKER_LOG=1 packer build -var "aws_access_key=$aws_access_key" -var "aws_secret_key=$aws_secret_key" -var "hostuser=$USER" build.json
+PACKER_LOG=1 ./packer build -var "aws_access_key=$aws_access_key" -var "aws_secret_key=$aws_secret_key" -var "hostuser=$USER" build.json
+cd ..
+
 docker run --name bf-aws-dev -v /home/$USER/code:/root/code -it local:aws-dev /bin/bash
