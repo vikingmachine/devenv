86d33a50d76ba927827af6a48645504d19d365c3
Thomas Finstad Larsen <thomas.finstad.larsen@gmail.com>
2018-12-04 14:51:02 +0100

    Move to python3 for awscli aswell

--- configure.sh
+++ configure.sh
@@ -6,8 +6,8 @@
 terraver=0.11.10
 
 apt update
-apt -y install bash-completion curl nano unzip wget groff openssl python python-pip python3 python3-pip openssh-client git python-virtualenv gcc sudo kpcli
-pip install awscli --upgrade
+apt -y install bash-completion curl nano unzip wget groff openssl python3 python3-pip openssh-client git python-virtualenv gcc sudo kpcli
+pip3 install awscli --upgrade
 wget https://releases.hashicorp.com/terraform/"$terraver"/terraform_"$terraver"_linux_amd64.zip
 wget https://raw.githubusercontent.com/basefarm/aws-session-tool/master/session-tool.sh
 unzip terraform_"$terraver"_linux_amd64.zip
